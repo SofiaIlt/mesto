ad7247fdc618acb4026ef2f3ba866ebad9f81c06
Sofia Iltner <sofia.fox8@mail.ru>
2022-08-11 19:45:15 +0300

    review changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,20 @@ let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
 let editButton = profile.querySelector('.profile__edit-button');
 let addButton = profile.querySelector('.profile__add-button');
-let formEdit = document.querySelector('.form');
-let formName = formEdit.querySelector('.form__name');
-let formJob = formEdit.querySelector('.form__job');
+let formEdit = document.querySelector('.popup__cover');
+let formCover = document.querySelector('.popup');
+let formName = formEdit.querySelector('input[name="form__name"]');
+let formJob = formEdit.querySelector('input[name="form__job"]');
 let safeButton = formEdit.querySelector('.form__button');
-let closeButton = formEdit.querySelector('.form__close-icon');
+let closeButton = formEdit.querySelector('.popup__close-icon');
 let header = document.querySelector('.header');
 let elements = document.querySelector('.elements');
 let likeButton = elements.querySelector('.element__like-button');
 let footer = document.querySelector('.footer');
 
 function openForm() {
-    formEdit.style.display = "block";
-    header.style.opacity = "0.5";
-    profile.style.opacity = "0.5";
-    elements.style.opacity = "0.5";
-    footer.style.opacity = "0.5";
+    formEdit.style.display = "flex";
+    formCover.style.display = "block";
 }
 
 function safeForm() {
@@ -32,19 +30,16 @@ function safeForm() {
 
 function closeForm() {
     formEdit.style.display = "none";
-    header.style.opacity = "";
-    profile.style.opacity = "";
-    elements.style.opacity = "";
-    footer.style.opacity = "";
+    formCover.style.display = "none";
 }
 
 function like(btn) {
     if (btn.target.classList.contains('element__like-button')) {
-        if (btn.target.classList.contains('active')) {
-            btn.target.classList.remove('active');
+        if (btn.target.classList.contains('element__like-button_active')) {
+            btn.target.classList.remove('element__like-button_active');
         }
         else {
-            btn.target.classList.add('active');
+            btn.target.classList.add('element__like-button_active');
         }
     }
 }
